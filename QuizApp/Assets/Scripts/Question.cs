d2b9abf23a911566160e14a8ced928c6ca9dbce8
Shamim Akhtar <shamim.akhtar@gmail.com>
2023-01-09 11:15:14 +0800

    Added the quiz manager script

---
+++ QuizApp/Assets/Scripts/Question.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question
+{
+  public string id;
+  public string question;
+  public List<string> choices = new List<string>();
+  public int correctAnswerid;
+  public string category;
+
+  // The below function is for testing. We are hardcoding a set 5
+  // question.
+
+  static public List<Question> CreateQuestions()
+  {
+    List<Question> questions = new List<Question>();
+
+    Question q1 = new Question();
+    q1.id = "1";
+    q1.question = "What is the capital of Malaysia?";
+    q1.choices.Add("Bangkok");
+    q1.choices.Add("Singapore");
+    q1.choices.Add("Kuala Lumpur");
+    q1.choices.Add("Hanoi");
+    q1.category = "Geography";
+    q1.correctAnswerid = 2;
+
+    Question q2 = new Question();
+    q2.id = "2";
+    q2.question = "How many islands are there in Singapore?";
+    q2.choices.Add("4");
+    q2.choices.Add("16");
+    q2.choices.Add("20");
+    q2.choices.Add("64");
+    q2.category = "Trivia";
+    q2.correctAnswerid = 3;
+
+    Question q3 = new Question();
+    q3.id = "3";
+    q3.question = "What is the square root of 144?";
+    q3.choices.Add("12");
+    q3.choices.Add("13");
+    q3.choices.Add("14");
+    q3.choices.Add("11");
+    q3.category = "Math";
+    q3.correctAnswerid = 0;
+
+    Question q4 = new Question();
+    q4.id = "4";
+    q4.question = "Whose nose grew longer when they lied?";
+    q4.choices.Add("Harry Potter");
+    q4.choices.Add("Pinnochio");
+    q4.choices.Add("Cinderella");
+    q4.choices.Add("Spiderman");
+    q4.category = "Trivia";
+    q4.correctAnswerid = 1;
+
+    Question q5 = new Question();
+    q5.id = "5";
+    q5.question = "Who is the author of Pride and Prejudice?";
+    q5.choices.Add("J K Rowling");
+    q5.choices.Add("JRR Tolkein");
+    q5.choices.Add("Barrack Obama");
+    q5.choices.Add("Jane Austen");
+    q5.category = "Trivia";
+    q5.correctAnswerid = 3;
+
+    return questions;
+  }
+}
--- QuizApp/Assets/Scripts/QuizManager.cs
+++ QuizApp/Assets/Scripts/QuizManager.cs
@@ -1,18 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuizManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  [SerializeField]
+  Text _totalScoreText;
+  [SerializeField]
+  Text _countdownTimerText;
+  [SerializeField]
+  Text _progressText;
+  [SerializeField]
+  Text _questionText;
+  [SerializeField]
+  Text _optionAText;
+  [SerializeField]
+  Text _optionBText;
+  [SerializeField]
+  Text _optionCText;
+  [SerializeField]
+  Text _optionDText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  [SerializeField]
+  RectTransform _panelQuiz;
+  [SerializeField]
+  RectTransform _panelCorrectAnswer;
+  [SerializeField]
+  RectTransform _panelInCorrectAnswer;
+  [SerializeField]
+  Button _nextButton;
+  [SerializeField]
+  Text _correctAnswerText;
+  [SerializeField]
+  Text _scoreText;
+
+  List<Question> _questions;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    LoadQuestions();
+  }
+
+  void LoadQuestions()
+  {
+    _questions = Question.CreateQuestions();
+  }
+
+  void ShowQuestion(int id)
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void OnClickNext()
+  {
+
+  }
+  public void OnClickOptionA()
+  {
+
+  }
+  public void OnClickOptionB()
+  {
+
+  }
+  public void OnClickOptionC()
+  {
+
+  }
+  public void OnClickOptionD()
+  {
+
+  }
 }
